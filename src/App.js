c7f81c3671cba93bbd7660842592578499755c46
npx <bhuvanagiriankammarao@gmail.com>
2024-03-14 18:58:37 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ function App() {
               <Navbar />
               <hr></hr>
               <Routes>
-                <Route path={`${PATH}/`}  element={<Products />} />
-                <Route path={`${PATH}/order`} element={<Orders />} />
+                <Route path={`${PATH}/`} element={<Products />} />
+                <Route path={`${PATH}/order`} index element={<Orders />} />
                 <Route path={`${PATH}/cart`} index element={<Cart />} />
               </Routes>
               <Footer />
