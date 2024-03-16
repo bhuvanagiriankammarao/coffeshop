6b0b4ba86451c93755274d5965b92ba16231f35c
npx <bhuvanagiriankammarao@gmail.com>
2024-03-16 16:07:55 +0530

    commit

--- src/components/product/Products.js
+++ src/components/product/Products.js
@@ -4,14 +4,7 @@ import products from "./products.json"
 import "./Product.css"
 import AddButton from "./AddButton.js";
 export default function Products() {
-  const { cartItems, setCartItems, } = useContext(AppContext);
-   const updateCart = (id, qty) => {
-    setCartItems((prev) => ({ ...prev, [id]: qty }));
-  };  
-
-  const addtoCart = (id) => {
-    setCartItems((prev) => ({ ...prev, [id]: (prev[id] ?? 0) + 1 }));               
-  };
+  const { cartItems, setCartItems } = useContext(AppContext);
   const PATH = process.env.REACT_APP_PATH
   // const addtoCart = (id) => {
   //   setCartItems((prev) => ({ ...prev, [id]: (prev[id] ?? 0) + 1 }));
@@ -22,13 +15,13 @@ export default function Products() {
       {products &&
         products.map((value) => (
           <div key={value.id} className="Product-items">
-            <div><img className="Product-img" src={value.image} alt={value.name}/></div>
+            <div><img className="Product-img" src={`${PATH}${value.image}`} alt={value.name}/></div>
             <h3>{value.name}</h3>
             <p style={{textAlign:"justify",padding:"10px"}}>{value.desc}</p>
             <div className="priceBtn">
             <div className="priceTxt">₹{value.price}</div>
-            {/* <div className="Product-button"><AddButton id={value.id}/></div> */}
-            <button className="Product-button" onClick={() => addtoCart(value.id)}>Add</button> 
+            <div className="Product-button"><AddButton id={value.id}/></div>
+            {/* <button className="Product-button" onClick={() => addtoCart(value.id)}>Add</button> */}
             </div>
           </div>
         ))}
